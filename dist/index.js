17c15023f633ba90f6a45cd770b485891cf5e809
Akash Askoolum <akash1810@gmail.com>
2021-09-24 09:07:03 +0100

    chore: Run lint and prettier across the code base

--- dist/index.js
+++ dist/index.js
@@ -40,22 +40,22 @@ const wait_1 = __nccwpck_require__(817);
 function run() {
     return __awaiter(this, void 0, void 0, function* () {
         try {
-            const ms = core.getInput('milliseconds');
+            const ms = core.getInput("milliseconds");
             core.debug(`Waiting ${ms} milliseconds ...`); // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
             core.debug(new Date().toTimeString());
             yield (0, wait_1.wait)(parseInt(ms, 10));
             core.debug(new Date().toTimeString());
-            core.setOutput('time', new Date().toTimeString());
+            core.setOutput("time", new Date().toTimeString());
         }
         catch (error) {
             const errorMessage = (err) => {
-                if (typeof err === 'string') {
+                if (typeof err === "string") {
                     return err;
                 }
                 if (err instanceof Error) {
                     return err.message;
                 }
-                return 'Internal Error';
+                return "Internal Error";
             };
             core.setFailed(errorMessage(error));
         }
@@ -83,11 +83,11 @@ Object.defineProperty(exports, "__esModule", ({ value: true }));
 exports.wait = void 0;
 function wait(milliseconds) {
     return __awaiter(this, void 0, void 0, function* () {
-        return new Promise(resolve => {
+        return new Promise((resolve) => {
             if (isNaN(milliseconds)) {
-                throw new Error('milliseconds not a number');
+                throw new Error("milliseconds not a number");
             }
-            setTimeout(() => resolve('done!'), milliseconds);
+            setTimeout(() => resolve("done!"), milliseconds);
         });
     });
 }
--- src/main.ts
+++ src/main.ts
@@ -1,29 +1,29 @@
-import * as core from '@actions/core'
-import {wait} from './wait'
+import * as core from "@actions/core";
+import { wait } from "./wait";
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
+    const ms: string = core.getInput("milliseconds");
+    core.debug(`Waiting ${ms} milliseconds ...`); // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    core.debug(new Date().toTimeString());
+    await wait(parseInt(ms, 10));
+    core.debug(new Date().toTimeString());
 
-    core.setOutput('time', new Date().toTimeString())
+    core.setOutput("time", new Date().toTimeString());
   } catch (error) {
     const errorMessage = (err: unknown) => {
-      if (typeof err === 'string') {
-        return err
+      if (typeof err === "string") {
+        return err;
       }
       if (err instanceof Error) {
-        return err.message
+        return err.message;
       }
-      return 'Internal Error'
-    }
+      return "Internal Error";
+    };
 
-    core.setFailed(errorMessage(error))
+    core.setFailed(errorMessage(error));
   }
 }
 
-void run()
+void run();
