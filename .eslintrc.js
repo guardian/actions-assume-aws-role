613f9c875a47c9c52b7bfb5006233be4ff05e747
Akash Askoolum <akash1810@gmail.com>
2021-09-24 09:05:30 +0100

    chore: Lint the tests too

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,17 @@
 module.exports = {
   env: {
     node: true,
-    es6: true
+    es6: true,
   },
-  extends: '@guardian/eslint-config-typescript',
-  parser: '@typescript-eslint/parser',
+  extends: "@guardian/eslint-config-typescript",
+  parser: "@typescript-eslint/parser",
   parserOptions: {
     ecmaVersion: 12,
     tsconfigRootDir: __dirname,
-    sourceType: 'module'
+    project: ["./tsconfig.eslint.json"],
+    sourceType: "module",
   },
-  plugins: ['@typescript-eslint'],
+  plugins: ["@typescript-eslint"],
   root: true,
-  ignorePatterns: ['**/*.js', 'node_modules']
-}
+  ignorePatterns: ["**/*.js", "node_modules"],
+};
